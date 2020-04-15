277237bde0fed3d41314e5a50d89e7229a5a8c4a
utkazu <hiroshimazeal@gmail.com>
2020-04-16 00:25:57 +0900

    create index page and solve n+1 problem

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,21 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   def search
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   get 'search', to: 'tasks#search'
   get 'result', to: 'tasks#result'
-  resources :tasks, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :tasks, only: [:new, :create, :edit, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
